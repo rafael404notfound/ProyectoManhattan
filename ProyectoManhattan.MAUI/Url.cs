f677ebdde2bebbf76540581260723f7974cce6a9
Rafael <rafasuarezdev@gmail.com>
2024-10-06 08:13:23 +0200

    coolify without maui2

--- ProyectoManhattan.MAUI/Url.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProyectoManhattan.BlazorComponents
-{
-    public static class UrlGetter
-    {
-        public static readonly string Url = "http://5.189.137.10/";
-        //public static readonly string Url = "https://localhost:5000/";
-    }
-}
