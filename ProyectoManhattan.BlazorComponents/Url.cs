c062ce3d847b114da301d1342058eb09c9a2c2df
Rafael <rafasuarezdev@gmail.com>
2024-11-02 05:10:35 +0100

    ApplyMigrationsExtensionOnProgramCS6

--- ProyectoManhattan.BlazorComponents/Url.cs
+++ ProyectoManhattan.BlazorComponents/Url.cs
@@ -3,7 +3,7 @@
     public static class UrlGetter
     {
         //public static readonly string Url = "http://5.189.137.10/";
-        public static readonly string Url = "http://212.47.66.68:5000/";
+        public static readonly string Url = "http://212.47.66.68:5001/";
         //public static readonly string Url = "https://localhost:5000/";
     }
 }
--- ProyectoManhattan.BlazorComponents/obj/Debug/net8.0/ProyectoManhattan.BlazorComponents.AssemblyInfo.cs
+++ ProyectoManhattan.BlazorComponents/obj/Debug/net8.0/ProyectoManhattan.BlazorComponents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoManhattan.BlazorComponents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006c27450323205cdb09479b77ee9575bb785137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f0aa5e1f81ae2698a1537d97e061bfa707b148e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoManhattan.BlazorComponents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoManhattan.BlazorComponents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
