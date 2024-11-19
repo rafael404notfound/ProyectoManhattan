c189afcd411cc5c034accdb1f2560483809ed884
Rafael <rafasuarezdev@gmail.com>
2024-11-19 06:25:35 +0100

    setcookies loops while cookies are invalid

--- ProyectoManhattan.Application/IJwtGetter/PlaywrightJwtGetter.cs
+++ ProyectoManhattan.Application/IJwtGetter/PlaywrightJwtGetter.cs
@@ -39,18 +39,45 @@ namespace ProyectoManhattan.Application
             foreach (var cookie in await browser.Contexts.First().CookiesAsync())
             {
                 cookieContainer.Add(new System.Net.Cookie(cookie.Name, cookie.Value, cookie.Path, cookie.Domain));
-                Console.WriteLine($"Writing cookier: {cookie.Name}, {cookie.Path}, {cookie.Domain} -> {cookie.Value} ");                
+                Console.WriteLine($"Writing cookie: {cookie.Name}, {cookie.Path}, {cookie.Domain} -> {cookie.Value} ");                
             }
+            await browser.CloseAsync();
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            await SetCookies();
+        { 
+            while(CookiesAreInvalid())
+            {
+                await SetCookies();
+            }   
+
             using var timer = new PeriodicTimer(Period);
 
             while(!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
             {
-                await SetCookies();
+                int tries = 0;
+                while(CookiesAreInvalid() && tries < 10)
+                {    
+                    tries ++;                
+                    await SetCookies();
+                }                
             }
+        }
+
+        private bool CookiesAreInvalid()
+        {
+            if (cookieContainer is not null)
+            {
+                foreach(var cookie in cookieContainer.GetAllCookies().ToList())
+                {
+                    if(cookie.Name == "sap-contextid")
+                    {
+                        Console.WriteLine("------------> Cookies are valid");
+                        return false;
+                    }
+                }    
+            }
+            Console.WriteLine("------------> Cookies are invalid");                    
+            return true;
         }
     }
 }
